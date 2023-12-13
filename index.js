a9cd977c00fed53a3f6714e535326072a6ae2617
Jillo Wario <willywario0@gmail.com>
2023-12-13 07:05:17 +0300

    Done with Assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+const destructivelyAppendCat = () => {
+  cats.push("Ralph");
+};
+
+const destructivelyPrependCat = () => {
+  cats.unshift("Bob");
+};
+const destructivelyRemoveFirstCat = () => {
+  cats.shift();
+};
+const destructivelyRemoveLastCat = () => {
+  cats.pop();
+};
+
+const appendCat = () => {
+  const newCatsArray = [...cats, "Broom"];
+  return newCatsArray;
+};
+const prependCat = () => {
+  const newCatsArray = ["Arnold", ...cats];
+  return newCatsArray;
+};
+
+const removeFirstCat = () => {
+  const newCats = cats.slice(1);
+  return newCats;
+};
+const removeLastCat = () => {
+  const newCats = cats.slice(0, -1);
+  return newCats;
+};
